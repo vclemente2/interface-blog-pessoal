21ef81f5bda14bb1d090327c8392f87e6e533b1c
Vinicius Bastos <vclemente2@hotmail.com>
2023-11-12 13:39:58 -0300

    Implementação das páginas de Login e Cadastro.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,22 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route
-          path="/home"
-          element={
-            <>
-              <Navbar />
-              <Home />
-              <Footer />
-            </>
-          }
-        ></Route>
+        <Route path="/cadastro" Component={Cadastro}></Route>
+        <Route path="/login" Component={Login}></Route>
+        <Route path="/home" Component={Home}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
